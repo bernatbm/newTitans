c054c3442e4a9a4121e7d17dfd562323d5efb691
GerardCamell <gerard_bcn_cabo@hotmail.es>
2025-04-13 12:47:55 +0200

    Tres formularis fets

--- webApp/app/admin/panelAdministrador.php
+++ webApp/app/admin/panelAdministrador.php
@@ -177,7 +177,7 @@ session_start();
 
                             <div class="pa-form-aeropuerto-origen">
                                 <label for="aeropuerto-origen">Aeropuerto de destino:</label>
-                                <select class="pa-input-aeropuerto-origen" id="aeropuerto-destino" name="id_destino" required>
+                                <select class="pa-input-aeropuerto-origen aeropuerto-select" name="id_destino" required>
                                     <option value="" disabled selected>Selecciona un aeropuerto</option>
                                 </select>
                             </div>
@@ -199,47 +199,49 @@ session_start();
                                 <label for="email-cliente">Email:</label>
                                 <input class="pa-input-email email-cliente" type="email" name="email_cliente" required>
                             </div>
+                            
 
                             <div class="pa-hotel-aeropuerto-submit">
                                 <button class="pa-btn-cambiar-trayecto" style="display: none;">Volver a selección de trayecto</button>
                                 <button class="pa-hotel-aeropuerto-button" id="hotel-aeropuerto" type="submit">Confirmar reserva</button>
                             </div>
-
                             <!---------------------- Formulario Opcional Usuario------------------------>
                             <div class="datos-viajero-adicionales" style="display: none;">
-                                <div>
-                                    <label>Nombre:</label>
-                                    <input class="pa-form-opcional" type="text" name="nombre" id="nombre" >
+                                    <div>
+                                        <label>Nombre:</label>
+                                        <input class="pa-form-opcional" type="text" name="nombre" id="nombre" >
+                                    </div>
+                                    <div>
+                                        <label>Apellido 1:</label>
+                                        <input class="pa-form-opcional" type="text" name="apellido1" id="apellido1" >
+                                    </div>
+                                    <div>
+                                        <label>Apellido 2:</label>
+                                        <input class="pa-form-opcional" type="text" name="apellido2" id="apellido2">
+                                    </div>
+                                    <div>
+                                        <label>Dirección:</label>
+                                        <input class="pa-form-opcional" type="text" name="direccion" id="direccion" >
+                                    </div>
+                                    <div>
+                                        <label>Código Postal:</label>
+                                        <input class="pa-form-opcional" type="text" name="codigoPostal" id="codigoPostal" >
+                                    </div>
+                                    <div>
+                                        <label>Ciudad:</label>
+                                        <input class="pa-form-opcional" type="text" name="ciudad" id="ciudad" >
+                                    </div>
+                                    <div>
+                                        <label>País:</label>
+                                        <input class="pa-form-opcional" type="text" name="pais" id="pais" >
+                                    </div>
+                                    <div>
+                                        <label>Contraseña:</label>
+                                        <input class="pa-form-opcional" type="password" name="password" id="password" >
+                                    </div>
                                 </div>
-                                <div>
-                                    <label>Apellido 1:</label>
-                                    <input class="pa-form-opcional" type="text" name="apellido1" id="apellido1" >
-                                </div>
-                                <div>
-                                    <label>Apellido 2:</label>
-                                    <input class="pa-form-opcional" type="text" name="apellido2" id="apellido2">
-                                </div>
-                                <div>
-                                    <label>Dirección:</label>
-                                    <input class="pa-form-opcional" type="text" name="direccion" id="direccion" >
-                                </div>
-                                <div>
-                                    <label>Código Postal:</label>
-                                    <input class="pa-form-opcional" type="text" name="codigoPostal" id="codigoPostal" >
-                                </div>
-                                <div>
-                                    <label>Ciudad:</label>
-                                    <input class="pa-form-opcional" type="text" name="ciudad" id="ciudad" >
-                                </div>
-                                <div>
-                                    <label>País:</label>
-                                    <input class="pa-form-opcional" type="text" name="pais" id="pais" >
-                                </div>
-                                <div>
-                                    <label>Contraseña:</label>
-                                    <input class="pa-form-opcional" type="password" name="password" id="password" >
-                                </div>
-                            </div>
+
+                           
                         </form>
 
 
@@ -261,42 +263,49 @@ session_start();
                                     <h3 class="pa-titulo-ida-y-vuelta">Ida</h3>
 
                                     <div class="pa-form-dia-llegada">
-                                        <label for="dia-llegada">Dia de llegada:</label>
-                                        <input class="pa-input-dia-llegada" type="date" id="dia-llegada" name="dia-llegada" required>
+                                        <label for="fecha-llegada">Dia de llegada:</label>
+                                        <input class="pa-input-dia-llegada" type="date" id="fecha-llegada" name="fecha_llegada" required>
                                     </div>
+
                                     <div class="pa-form-hora-llegada">
                                         <label for="hora-llegada">Hora de llegada:</label>
                                         <input class="pa-input-hora-llegada" type="time" id="hora-llegada" name="hora_llegada" required>
                                     </div>
                                     <div class="pa-form-numero-vuelo">
                                         <label for="numero-vuelo">Numero de vuelo:</label>
-                                        <input class="pa-input-numero-vuelo" type="text" id="numero-vuelo-ida" name="numero_vuelo_ida">
+                                        <input class="pa-input-numero-vuelo" type="text" id="numero-vuelo-ida" name="numero_vuelo_entrada" required>
                                     </div>
                                     <div class="pa-form-aeropuerto-origen">
                                         <label for="aeropuerto-origen" >Aeropuerto de origen:</label>
                                         <input class="pa-input-aeropuerto-origen" type="text" id="aeropuerto-origen" name="aeropuerto_origen" required>
                                     </div>
-                                    <div class="pa-form-hotel-destino">
-                                        <label for="hotel-destino">Hotel de destino:</label>
-                                        <input class="pa-input-hotel-destino" type="text" id="hotel-destino" name="hotel_destino" required>
-                                    </div>
-                                    <div class="pa-form-numero-viajeros">
-                                        <label for="numero-viajeros">Número de viajeros:</label>
-                                        <select class="pa-select-numero-viajeros" id="numero-viajeros" name="numero_viajeros" required>
-                                            <option value="" disabled selected>Selecciona</option>
-                                            <option value="1">1 viajero</option>
-                                            <option value="2">2 viajeros</option>
-                                            <option value="3">3 viajeros</option>
-                                            <option value="4">4 viajeros</option>
-                                            <option value="5">5 viajeros</option>
-                                            <option value="6">6 viajeros</option>
+
+                                    <div class="pa-form-zona">
+                                        <label for="id-zona">Zona:</label>
+                                        <select class="pa-input-zona" id="id-zona" name="id_zona" required>
+                                            <option value="" disabled selected>Selecciona una zona</option>
+                                            <?php include '../controller/getZonas.php'; ?>
                                         </select>
                                     </div>
-                                    
-                                    <div class="pa-form-email">
-                                        <label for="email-cliente">Email:</label>
-                                        <input class="pa-input-email email-cliente" type="email" name="email_cliente" required>
+                                    <div class="pa-form-hotel-destino">
+                                        <label for="id-hotel">Hotel de destino:</label>
+                                        <select class="pa-select-hotel-destino" id="id-hotel" name="id_hotel" required>
+                                            <option  disabled selected>Primero selecciona una zona</option>
+                                        </select>
                                     </div>
+
+                                    <div class="pa-form-numero-viajeros">
+                                        <label for="numero-viajeros">Número de viajeros:</label>
+                                        <select class="pa-select-numero-viajeros" id="numero-viajeros" name="num_viajeros" required>
+                                        <option value="" disabled selected>Selecciona</option>
+                                        <?php
+                                            for ($i = 1; $i <= 8; $i++) {
+                                                echo "<option value='$i'>$i viajero" . ($i > 1 ? "s" : "") . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                                                
                                 </div>
 
                                 <!--Columna derecha-->
@@ -319,41 +328,64 @@ session_start();
                                         <label for="hora-recogida">Hora de recogida:</label>
                                         <input class="pa-input-hora-recogida" type="time" id="hora-recogida" name="hora_recogida" required>
                                     </div>
-                                    <div class="pa-form-hotel-recogida">
-                                        <label for="hotel-recogida">Hotel de recogida</label>
-                                        <input class="pa-input-hotel-recogida" type="text" id="hotel-recogida" name="hotel_recogida" required>
-                                    </div>
-                                    <div class="pa-form-aeropuerto-destino">
-                                        <label for="aeropuerto-destino">Aeropuerto de destino</label>
-                                        <input class="pa-input-aeropuerto-destino" type="text" id="aeropuerto-recogida" name="aeropuerto_recogia" required>
-                                    </div>
-                                    <div class="pa-form-numero-viajeros">
-                                        <label for="numero-viajeros">Número de viajeros:</label>
-                                        <select class="pa-select-numero-viajeros" id="numero-viajeros" name="numero_viajeros" required>
-                                            <option value="" disabled selected>Selecciona</option>
-                                            <option value="1">1 viajero</option>
-                                            <option value="2">2 viajeros</option>
-                                            <option value="3">3 viajeros</option>
-                                            <option value="4">4 viajeros</option>
-                                            <option value="5">5 viajeros</option>
-                                            <option value="6">6 viajeros</option>
+                                    
+                                    <div class="pa-form-aeropuerto-origen">
+                                        <label for="aeropuerto-origen">Aeropuerto de destino:</label>
+                                        <select class="pa-input-aeropuerto-origen aeropuerto-select" name="id_destino" required>
+                                            <option value="" disabled selected>Selecciona un aeropuerto</option>
                                         </select>
                                     </div>
                                     
+                                    
                                     <div class="pa-form-email">
                                         <label for="email-cliente">Email:</label>
-                                        <input class="pa-input-email" type="email" id="email-cliente" name="email_cliente" required>
+                                        <input class="pa-input-email email-cliente" type="email" name="email_cliente" required>
                                     </div>
 
-
                                 </div>
-
-                            
-
+                                
                                 <div class="pa-ida-vuelta-submit">
                                     <button id="btn-cambiar-trayecto" class="pa-btn-cambiar-trayecto" style="display: none;">Volver a selección de trayecto</button>
                                     <button class="pa-ida-vuelta-button" id="ida-vuelta" type="submit">Confirmar reserva</button>
                                 </div>
+
+                                <!---------------------- Formulario Opcional Usuario------------------------>
+                                <div class="datos-viajero-adicionales" style="display: none;">
+                                    <div>
+                                        <label>Nombre:</label>
+                                        <input class="pa-form-opcional" type="text" name="nombre" id="nombre" >
+                                    </div>
+                                    <div>
+                                        <label>Apellido 1:</label>
+                                        <input class="pa-form-opcional" type="text" name="apellido1" id="apellido1" >
+                                    </div>
+                                    <div>
+                                        <label>Apellido 2:</label>
+                                        <input class="pa-form-opcional" type="text" name="apellido2" id="apellido2">
+                                    </div>
+                                    <div>
+                                        <label>Dirección:</label>
+                                        <input class="pa-form-opcional" type="text" name="direccion" id="direccion" >
+                                    </div>
+                                    <div>
+                                        <label>Código Postal:</label>
+                                        <input class="pa-form-opcional" type="text" name="codigoPostal" id="codigoPostal" >
+                                    </div>
+                                    <div>
+                                        <label>Ciudad:</label>
+                                        <input class="pa-form-opcional" type="text" name="ciudad" id="ciudad" >
+                                    </div>
+                                    <div>
+                                        <label>País:</label>
+                                        <input class="pa-form-opcional" type="text" name="pais" id="pais" >
+                                    </div>
+                                    <div>
+                                        <label>Contraseña:</label>
+                                        <input class="pa-form-opcional" type="password" name="password" id="password" >
+                                    </div>
+                                </div>
+
+                                
                             </div>
                         </form>
 
